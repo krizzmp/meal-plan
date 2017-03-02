afcca0b42e6c81d13b3277d2e26209b9f8167a27
krizzmp <krizzmp@gmail.com>
2017-03-02 14:20:24 +0100

    update

--- app/reducers/app.js
+++ app/reducers/app.js
@@ -1,12 +1,12 @@
 import { ADD_INGREDIENT, ADD_RECIPE, REM_RECIPE, UPDATE_INGREDIENT } from '../actions/app';
 import { combineReducers } from 'redux';
-import {append, findIndex, lensIndex, propEq, reject, set} from 'ramda';
+import { append, findIndex, lensIndex, propEq, reject, set } from 'ramda';
 const _ingredientList = [
-  { text: 'Spaghetti', value: 'someFirstValue', unit: 'stk', shopItems:[]},
-  { text: 'Basmati Rice', value: 'someFirstValue', unit: 'stk', shopItems:[] },
-  { text: 'Parmesan', value: 'someFirstValue', unit: 'stk', shopItems:[] },
-  { text: 'Egg', value: 'someFirstValue', unit: 'stk', shopItems:[] },
-  { text: 'Whole Canned Tomatoes', value: 'someFirstValue', unit: 'stk', shopItems:[] }
+  { text: 'Spaghetti', value: 'someFirstValue', unit: 'stk', shopItems: [] },
+  { text: 'Basmati Rice', value: 'someFirstValue', unit: 'stk', shopItems: [] },
+  { text: 'Parmesan', value: 'someFirstValue', unit: 'stk', shopItems: [] },
+  { text: 'Egg', value: 'someFirstValue', unit: 'stk', shopItems: [] },
+  { text: 'Whole Canned Tomatoes', value: 'someFirstValue', unit: 'stk', shopItems: [] }
 ];
 const _recipeList = [
   {
@@ -18,13 +18,15 @@ const _recipeList = [
 
 function ingredientList(state = _ingredientList, action) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ADD_INGREDIENT: {
       const { ingredient, unit } = action;
-      return [ ...state, { text: ingredient, value: '', unit, shopItems:[] } ];
-    case UPDATE_INGREDIENT:
+      return [ ...state, { text: ingredient, value: '', unit, shopItems: [] } ];
+    }
+    case UPDATE_INGREDIENT: {
       const { text, newIng } = action;
       const idx = findIndex(propEq('text', text), state);
       return set(lensIndex(idx), newIng, state);
+    }
     default:
       return state;
   }
@@ -32,12 +34,14 @@ function ingredientList(state = _ingredientList, action) {
 
 function recipeList(state = _recipeList, action) {
   switch (action.type) {
-    case ADD_RECIPE:
+    case ADD_RECIPE: {
       const { recipe } = action;
       return append(recipe, state);
-    case REM_RECIPE:
+    }
+    case REM_RECIPE: {
       const { recipeID } = action;
       return reject(propEq('title', recipeID), state);
+    }
     default:
       return state;
   }
