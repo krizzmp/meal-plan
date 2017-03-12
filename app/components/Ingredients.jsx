f1b5610893b3182ef877008ee949e94d5f936990
krizzmp <krizzmp@gmail.com>
2017-03-12 16:58:32 +0100

    update

--- app/components/Ingredients.jsx
+++ app/components/Ingredients.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { PropTypes as pt } from 'react';
 import { Subheader, List, ListItem } from 'material-ui';
-import { curry } from 'ramda';
 import { grey500 } from 'material-ui/styles/colors';
+import { setPropTypes } from 'recompose';
+import { curry, map } from 'ramda';
 const styles = { color: grey500, float: 'right' };
-const Ingredient = curry((p, tile) => (
+
+const ShopItemType = {};
+const IngredientType = { text: pt.string, shopItems: pt.arrayOf(pt.shape(ShopItemType)) };
+const c2pt = curry((pt, fn) => curry((p, tile) => fn(p, setPropTypes(pt)(tile))));
+
+const Ingredient = c2pt(IngredientType)((p, tile) => (
   <ListItem
     key={tile.text}
     primaryText={
@@ -12,13 +18,17 @@ const Ingredient = curry((p, tile) => (
     onClick={() => p.router.push(`/ingredients/${tile.text}`)}
   />
 ));
-export default p => (
-  <div id="content">
-    <div>
-      <List>
-        <Subheader>Ingredients</Subheader>
-        {p.ingredientList.map(Ingredient(p))}
-      </List>
+export default setPropTypes({
+  ingredientList: pt.arrayOf(pt.shape(IngredientType))
+})(function IngredientList(p) {
+  return (
+    <div id="content">
+      <div>
+        <List>
+          <Subheader>Ingredients</Subheader>
+          {map(Ingredient(p), p.ingredientList)}
+        </List>
+      </div>
     </div>
-  </div>
-);
+  );
+});
